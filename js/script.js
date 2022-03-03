e5af903ae729445e8127fadbbe57e80e1a16d97d
mstars_lab2_08 <mstars_lab2_08@mstars-lab2-08s-iMac.local>
2022-03-03 17:06:07 +0800

    update

--- js/script.js
+++ js/script.js
@@ -4,30 +4,51 @@
 // e.target.scrollingElement.scrollTop use that method when track scrolling amount
 
 function scrollDetect(e) {
-  console.log();
-}
+  let st = e.target.scrollingElement.scrollTop;
+  const navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", (event) => {
-  if () {
-
+  if (st > window.innerHeight / 1.5) {
+    navbar.classList.add("fixed-top");
+  } else {
+    navbar.classList.remove("fixed-top");
   }
-});
+}
+console.log(window.innerHeight);
 
 // Add scrollDetect callback function on window.addEventlistener('scroll', callback) Listener
+window.addEventListener("scroll", scrollDetect);
 
 // Define Play button variable
+const playButton = document.querySelector(".btn-video-player");
 
 // Create a modal with a single line of JavaScript: https://getbootstrap.com/docs/5.1/components/modal/#via-javascript
 // Define finsetModal then use Bootstrap modal Object request
+let finsetModal = new bootstrap.Modal(document.getElementById("finsetModal"), {
+  keyboard: false,
+});
 
 // Create Click Event Listener on Play button variable (you already created above)
 // add finsetModal.show() on callback function
 // Select '.modal-body' class from finsetModal variable then use innerHTML method and add Youtube embed code
-
+// playButton.addEventListener("click", function () {
+//   finsetModal.show();
+//   // console.log(finsetModal);
+//   finsetModal._element.querySelector(
+//     ".modal-body"
+//   ).innerHTML = `<iframe width="100%" height="600"`;
+// });
 // Create an XMLHttpRequest object
 // Create a callback function
 // Open a GET request and use data from ../data/company_intro.json
 // Send the request
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    let data = JSON.parse(xhr.responseText);
+  }
+};
+xhr.open("GET", "/data/company_intro.json");
+xhr.send();
 
 /*  Add subscription email action. When subscription POST request is successful, 
     change the email element and subscribe button into "Your subscription is successful" Text. 
